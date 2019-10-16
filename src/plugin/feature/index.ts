d05877e6b7885fb60187303ae8d39bba6a3ed4d0
irving <irving.zhao.dev@hotmail.com>
2019-10-16 09:26:33 +0800

    feature 安装

---
+++ src/plugin/feature/index.ts
@@ -0,0 +1,24 @@
+import {VueConstructor} from "vue";
+import DateFormatter from "./dateFormatter";
+import LoadingBar from "./loadingBar";
+
+export default function install(Vue: VueConstructor) {
+    Vue.filter("simpleDate", (val: Date | number) => {
+        return DateFormatter(val, "yyyy-MM-dd");
+    });
+
+    Vue.filter("fullDate", (val: Date | number) => {
+        return DateFormatter(val, "yyyy-MM-dd HH:mm:ss");
+    });
+
+    Vue.filter("dateFormat", (val: Date | number, pattern: string) => {
+        return DateFormatter(val, pattern);
+    });
+
+    Vue.$loadingBar = LoadingBar;
+    Vue.prototype.$loadingBar = LoadingBar;
+}
+
+export {
+    DateFormatter
+};
