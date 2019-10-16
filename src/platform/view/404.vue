ae9f06ed3be89854fe7834b15c94dd1e37efb831
irving <irving.zhao.dev@hotmail.com>
2019-10-16 11:19:18 +0800

    404 页面

---
+++ src/platform/view/404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <img src="../assets/404.svg" alt="">
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Page404 extends Vue {
+    }
+</script>
+
+<style scoped>
+    img {
+        display: block;
+        margin: 0 auto;
+        min-height: 500px;
+    }
+</style>
