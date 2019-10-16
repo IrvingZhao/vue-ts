3a644bc35b82c2d8c483a9e4cabc8aa954d8c9b7
irving <irving.zhao.dev@hotmail.com>
2019-10-16 09:26:19 +0800

    调整格式

--- src/plugin/feature/loadingBar/loading-bar.vue
+++ src/plugin/feature/loadingBar/loading-bar.vue
@@ -6,64 +6,64 @@
     </transition>
 </template>
 <script lang="ts">
-    import {Component, Prop, Vue} from "vue-property-decorator";
+import {Component, Prop, Vue} from "vue-property-decorator";
 
-    const prefixCls = "ivu-loading-bar";
+const prefixCls = "ivu-loading-bar";
 
-    @Component<LoadingBar>({
-        name: "XlbLoadingBar",
-    })
-    export default class LoadingBar extends Vue {
+@Component<LoadingBar>({
+    name: "XlbLoadingBar",
+})
+export default class LoadingBar extends Vue {
 
-        @Prop({type: String, default: "primary"})
-        private color!: string;
-
-        @Prop({type: String, default: "error"})
-        private failedColor!: string;
-
-        @Prop({type: Number, default: 2})
-        private height!: number;
-
-        public percent = 0;
-        public status = "success";
-        public show = false;
-
-        get classes() {
-            return `${prefixCls}`;
-        }
-
-        get innerClasses() {
-            return [
-                `${prefixCls}-inner`,
-                {
-                    [`${prefixCls}-inner-color-primary`]: this.color === "primary" && this.status === "success",
-                    [`${prefixCls}-inner-failed-color-error`]: this.failedColor === "error" && this.status === "error"
-                }
-            ];
-        }
-
-        get outerStyles() {
-            return {
-                height: `${this.height}px`
-            };
-        }
-
-        get styles() {
-            let style = {
-                width: `${this.percent}%`,
-                height: `${this.height}px`,
-                backgroundColor: "",
-            };
-
-            if (this.color !== "primary" && this.status === "success") {
-                style.backgroundColor = this.color;
-            }
-
-            if (this.failedColor !== "error" && this.status === "error") {
-                style.backgroundColor = this.failedColor;
-            }
-
-            return style;
-        }
+    get classes() {
+        return `${prefixCls}`;
     }
+
+    get innerClasses() {
+        return [
+            `${prefixCls}-inner`,
+            {
+                [`${prefixCls}-inner-color-primary`]: this.color === "primary" && this.status === "success",
+                [`${prefixCls}-inner-failed-color-error`]: this.failedColor === "error" && this.status === "error"
+            }
+        ];
+    }
+
+    get outerStyles() {
+        return {
+            height: `${this.height}px`
+        };
+    }
+
+    get styles() {
+        const style = {
+            width: `${this.percent}%`,
+            height: `${this.height}px`,
+            backgroundColor: "",
+        };
+
+        if (this.color !== "primary" && this.status === "success") {
+            style.backgroundColor = this.color;
+        }
+
+        if (this.failedColor !== "error" && this.status === "error") {
+            style.backgroundColor = this.failedColor;
+        }
+
+        return style;
+    }
+
+    public percent = 0;
+    public status = "success";
+    public show = false;
+
+    @Prop({type: String, default: "primary"})
+    private color!: string;
+
+    @Prop({type: String, default: "error"})
+    private failedColor!: string;
+
+    @Prop({type: Number, default: 2})
+    private height!: number;
+}
 </script>
