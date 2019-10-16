daeefc1ee8b39ac768f05f99d1facce5b20821e3
irving <irving.zhao.dev@hotmail.com>
2019-10-16 09:54:28 +0800

    更新filter创建位置

--- src/plugin/util/index.ts
+++ src/plugin/util/index.ts
@@ -2,20 +2,10 @@ import * as Util from "./util";
 import {VueConstructor} from "vue";
 
 export default function install(Vue: VueConstructor) {
-
+    Vue.$util = Util;
     Vue.prototype.$util = Util;
-
-    Vue.filter("simpleDate", (val: Date | number) => {
-        return Util.getSimpleDate(val);
-    });
-
-    Vue.filter("fullDate", (val: Date | number) => {
-        return Util.getFullDate(val);
-    });
-
-    Vue.filter("dateFormat", (val: Date | number, pattern: string) => {
-        return Util.dateFormat(val, pattern);
-    });
 }
 
-export * from "./util";
+export {
+    Util
+};
