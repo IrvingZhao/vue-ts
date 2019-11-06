c40aa9756612d2f739e22271df346f7d54394b4d
irving <irving.zhao.dev@hotmail.com>
2019-11-06 16:41:29 +0800

    添加启动配置

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,20 @@
 const host = "localhost";
-const port = 4000;
+const port = 3000;
 const baseUrl = "/";
 const outputDir = "dist";
 const assertDir = "static";
-const path = require("path");
-const proxy = {
-    "/api": {
-        target: "http://gh.yixiuhuo.com",
-        changeOrigin: true,               // needed for virtual hosted sites
-        ws: true,// proxy websockets
-        logLevel: 'debug',
-    }
-};
 
 module.exports = {
     publicPath: baseUrl,
     outputDir: outputDir,
     assetsDir: assertDir,
     devServer: {
-        clientLogLevel: "info",
+        clientLogLevel: 'info',
         quiet: true,
         host: host,
         port: port,
-        proxy: proxy,
+    },
+    configureWebpack: {
+        entry: "./example/main.ts"
     },
 };
