1c4d3e89607239aa27151e580b82a7f0bf886d60
irving <irving.zhao.dev@hotmail.com>
2019-11-06 15:24:06 +0800

    插件测试方法

---
+++ example/BasePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <xlb-base-page>
+        <template v-slot:title>测试标题</template>
+        <template v-slot:searchForm>
+            <input >
+            <button>查询</button>
+        </template>
+        <template v-slot:tools>
+            <button>操作A</button>
+            <button>操作B</button>
+        </template>
+        <template v-slot:content>
+            <p>内容区</p>
+        </template>
+        <template v-slot:footer>
+            底部区域
+        </template>
+    </xlb-base-page>
+</template>
+
+<script>
+    export default {
+        name: "BasePage",
+        data() {
+            return {
+                form: {
+                    text: ""
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
